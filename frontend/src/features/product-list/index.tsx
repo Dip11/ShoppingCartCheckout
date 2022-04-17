f86c268b6c27674fc5ee2a4fa86e630ab5f0fcbf
Dip11 <diptab11@gmail.com>
2022-04-18 00:38:54 +0900

    Adding ProductList  View Component

--- frontend/src/features/product-list/index.tsx
+++ frontend/src/features/product-list/index.tsx
@@ -1,8 +1,44 @@
-export default function ProductListView() {    
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { getProductListAsync, productList } from './productListStore';
+
+export default function ProductListView() {
+    const products = useAppSelector(productList);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(getProductListAsync());
+    }, [])
+    
     return (
         <>
             <div className="row">
-                <p>ProductList</p> 
+                <div className="col-md-12">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Product Code</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.map(item => {
+                                return <tr key={item.product_code}>
+                                    <td>{item.product_code}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}€</td>
+                                    <td>
+                                        <button className="btn btn-primary">
+                                            Add to Basket
+                                        </button>
+                                    </td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </table>
+                </div> 
             </div>
         </>
     )
