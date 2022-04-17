a8b00a7d94f9b2e3df170bc072256c9b06ea115e
Dip11 <diptab11@gmail.com>
2022-04-18 00:58:15 +0900

    product_code Cammel Case changes in product mock data in the backend

--- backend/src/mockdata/product.data.ts
+++ backend/src/mockdata/product.data.ts
@@ -2,17 +2,17 @@ import { ProductInterface } from "../interfaces/product.interface";
 
 export const products: ProductInterface[] = [
     {
-        product_code: "001",
+        productCode: "001",
         name: "Curry Sauce",
         price: 1.95
     },
     {
-        product_code: "002",
+        productCode: "002",
         name: "Pizza",
         price: 5.99
     },
     {
-        product_code: "003",
+        productCode: "003",
         name: "Men's T-Shirt",
         price: 25.00
     }
